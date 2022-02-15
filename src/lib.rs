6e0817a54e2be402049c3a9b7bad4ac74eb608ea
Adit Chauhan <chauhan.adit.98@gmail.com>
2022-02-15 10:41:50 +0530

    Documentaion and minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,77 @@
+//! Custom Panic Messages According to the error.
+//!
+//! Handles panics by calling a custom function using
+//! [`std::panic::set_hook`](https://doc.rust-lang.org/std/panic/fn.set_hook.html)
+//! and a Yaml File to generate the custom structs.
+//!
+//! This allows for seperate error messages for seperate error and also allows the user to run some simple fixes (if possible).
+//!
+//! ### Output Example
+//!
+//! Example of an API error's panic output
+//!
+//! ```txt
+//! The Program Crashed
+//!
+//! Error: There was an error during the API request
+//! It seems like an error that can be fixed by you!
+//! Please follow the following instructions to try and fix the Error
+//!
+//!     1: Try to check your Internet Connection.
+//!
+//! 	2: Check if your API request quota has been exhausted.
+//! 		1.  Instructions on how
+//! 		2.  to check
+//! 		3.  API quota
+//!
+//! If the error still persists
+//! Contact the Developer at xyz@wkl.com
+//! ```
+//! ### Code Example
+//! To replicate the above output you need to first create a yaml file as follows.
+//! ```txt
+//! API:
+//!   message: There was an error during the API request
+//!   fix instructions:
+//!       - Try to check your Internet Connection.
+//!       - Check if your API request quota has been exhausted.
+//!       - - Instructions on how
+//!         - to check
+//!         - API quota
+//! ```
+//! then you need to create the [build script](https://doc.rust-lang.org/cargo/reference/build-scripts.html) make sure userpanic is present in both dependencies and build dependencies in cargo.toml file
+//! ```toml
+//! [dependencies]
+//! userpanic = "0.1.0"
+//!
+//! [build-dependencies]
+//! userpanic = "0.1.0"
+//! ```
+//! and make build.rs file as follows
+//! ```
+//! fn main() {
+//!    println!("cargo:rerun-if-changed=errors.yaml");
+//!    println!("cargo:rerun-if-changed=build.rs");
+//!    userpanic::panic_setup!("errors.yaml"); // Enter the yaml file path here
+//! }
+//! ```
+//! This will create `panic_strucs.rs` file in src directory
+//! This file can be then imported and used with panic_any to display the custom panics
+//! ```
+//! mod panic_structs;
+//!
+//! use std::panic::panic_any;
+//! use crate::panic_structs::API;
+//!
+//! fn main(){
+//!     // This sets the custom hook for panic messages
+//!     userpanic::set_hooks(Some("If the error still persists\nContact the developer at xyz@wkl.com"));
+//!     // If None is passed then No developer info/message is shown.
+//!
+//!     panic_any(API);
+//! }
+//! ```
+
 use log::{debug, info};
 use std::fmt;
 use std::io::Write;
@@ -6,10 +80,16 @@ use std::panic::PanicInfo;
 use yaml_rust::{Yaml, YamlLoader};
 
 type StrList = [&'static [&'static str]];
+type Panicfn = Box<dyn Fn(&PanicInfo) + Sync + Send>;
 
 #[derive(Debug, Clone)]
+/// This Struct is auto generated from the yaml file
 pub struct UserPanic {
+    /// It describes the error
+    ///
+    /// If left empty then the program panics silently without giving any output
     pub error_msg: &'static str,
+    /// It contains the instructions to fix the error
     pub fix_instructions: Option<&'static StrList>,
 }
 impl fmt::Display for UserPanic {
@@ -17,13 +97,14 @@ impl fmt::Display for UserPanic {
         if self.error_msg == "" {
             return write!(f, "");
         }
-        let mut s = String::from("Whoops!\nUnrecoverable error occerred\n\n");
+        // Need something better than "The Program Crashed" :(
+        let mut s = String::from("The Program Crashed\n\n");
         if self.fix_instructions.is_none() {
             s += &format!("Error: {}", self.error_msg);
-            s += "\nIt seems like an error that can't be fixed by you!\nPlease follow the following instructions to submit a Bug report to Developer\n";
+            s += "\nIt seems like an error that can't be fixed by you!\nPlease submit a Bug report to Developer\n";
         } else {
             s += &format!("Error: {}", self.error_msg);
-            s += "\nIt seems like an error that can be fixed by you!\nPlease follow the following instructions to try and fix the bug\n";
+            s += "\nIt seems like an error that can be fixed by you!\nPlease follow the following instructions to try and fix the Error\n";
             let insts = self.fix_instructions.as_ref().unwrap();
             let mut i = 1;
             for inst in *insts {
@@ -42,51 +123,43 @@ impl fmt::Display for UserPanic {
         write!(f, "{}", s)
     }
 }
-
+/// This function is used to set custom panic function
+/// Use this to use the custom hooks and set up the developer message
 pub fn set_hooks(developer: Option<&'static str>) {
-    let _org = panic::take_hook();
+    let org: Panicfn = panic::take_hook();
     if let Some(dev) = developer {
         // Used if The developer provides custom info
-        //
         panic::set_hook(Box::new(move |pan_inf| {
-            panic_func(pan_inf);
+            panic_func(pan_inf, &org);
             eprintln!("{}", dev);
         }))
     } else {
         // Used if Developer doesn't want info to be shown.
-        panic::set_hook(Box::new(panic_func));
+        panic::set_hook(Box::new(move |pan_inf| {
+            panic_func(pan_inf, &org);
+        }));
     }
 }
-
-fn panic_func(panic_info: &PanicInfo) {
+// The panic function
+fn panic_func(panic_info: &PanicInfo, original: &Panicfn) {
     match panic_info.payload().downcast_ref::<UserPanic>() {
         Some(err) => {
             if err.error_msg != "" {
                 eprintln!("{}", err);
             }
         }
-        None => match panic_info.payload().downcast_ref::<&str>() {
-            Some(err) => eprintln!("{}", err),
-            None => match panic_info.payload().downcast_ref::<String>() {
-                Some(err) => eprintln!("{}", err),
-                None => eprintln!("Unrecognized ERROR format"),
-            },
-        },
+        // Default to original panic routine if downcast_ref fails
+        None => original(panic_info),
     }
 }
-
+// Returns the auto generated rust code
 fn read_from_yml(yaml: String) -> String {
     debug!("Started Reading the yaml string");
     let mut file = "use userpanic::UserPanic;\n".to_string();
     let yaml = YamlLoader::load_from_str(&yaml).unwrap();
     let structs = &yaml[0];
     if let Yaml::Hash(hash) = structs {
-        let l = hash.keys().clone();
-
-        info!("Found Hash with keys");
-        for ll in l {
-            debug!("{:?}", ll);
-        }
+        info!("Found Hash");
         // for test case keys -> foo bar
         for (key, val) in hash {
             let st_name = key.as_str().unwrap();
@@ -100,6 +173,8 @@ fn read_from_yml(yaml: String) -> String {
     }
     file
 }
+// Helper function for read_from_yml
+// Idk why I named it this it doesn't make sense
 fn get_err_msg(hash: &Yaml) -> String {
     let print_arr = |arr: &Vec<Yaml>| -> String {
         let mut s = String::new();
@@ -157,23 +232,19 @@ fn get_err_msg(hash: &Yaml) -> String {
 }
 
 #[macro_export]
+/// Macro to be used in build script
+/// Only yaml file path or both yaml and output rust file can be provided
 macro_rules! panic_setup {
     ($file_path:expr) => {
-        userpanic::panic_setup_default($file_path);
+        userpanic::panic_setup_function($file_path, "src/panic_structs.rs");
     };
     ($file_path:expr,$file_out:expr) => {
-        userpanic::panic_setup_with_path($file_path, $file_out);
+        userpanic::panic_setup_function($file_path, $file_out);
     };
 }
-
-pub fn panic_setup_default(path: &str) {
-    let file_str = std::fs::read_to_string(path).expect("Failed to read yaml file");
-    let s = read_from_yml(file_str);
-    let mut fp =
-        std::fs::File::create("src/panic_structs.rs").expect("failed to create output file");
-    write!(&mut fp, "{}", s).expect("failed to write to file");
-}
-pub fn panic_setup_with_path(path_from: &str, path_to: &str) {
+/// Not intended to be used directly and to be called by panic_setup! macro
+/// The main build script function
+pub fn panic_setup_function(path_from: &str, path_to: &str) {
     let file_str = std::fs::read_to_string(path_from).expect("Failed to read yaml file");
     let s = read_from_yml(file_str);
     let mut fp = std::fs::File::create(path_to).expect("failed to create output file");
@@ -218,12 +289,5 @@ bar:
 ";
         let s = read_from_yml(s.to_string());
         assert_eq!("use userpanic::UserPanic;\npub const foo:UserPanic = UserPanic {error_msg:\"this is the main error\",fix_instructions:Some(&[&[\"first\",\"in first\",\"in first second\"],&[\"second\",\"second first\",\"second second\"],&[\"third\"],]),};pub const bar:UserPanic = UserPanic {error_msg:\"This is un fixable error\",fix_instructions: None,};", s);
-    }
-
-    #[test]
-    fn from_file() {
-        let file_str = std::fs::read_to_string("s.yaml").unwrap();
-        let s = read_from_yml(file_str);
-        assert_eq!("use userpanic::UserPanic;\npub const ERROR:UserPanic = UserPanic {error_msg:\"This is an error\",fix_instructions:Some(&[&[\"Main Cause\",\"Main Cause 1\",\"Main Cause 2\",\"Main Cause 3\"],&[\"Secondary Cause\"],&[\"Tert Cause\",\"Tert Cause 1\",\"Tert Cause 2\"],]),};pub const SILENT:UserPanic = UserPanic {error_msg:\"\",fix_instructions: None,};", s);
     }
 }
